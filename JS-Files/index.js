47fe855a0a92704b4895a7335fe1b9de7aa681dd
MohamedAmin324 <mohamedaminebofficiel@gmail.com>
2024-01-20 16:37:49 +0100

    Make the navbar responsive

--- JS-Files/index.js
+++ JS-Files/index.js
@@ -1,16 +1,27 @@
 "use strict";
 
-import { selectAll, toggleVisibility } from "./utility-functions.js";
+import { selectAll, toggleVisibility, changeLayout } from "./utility-functions.js";
 
 const toggleButtons = selectAll(".close-menu-btn", ".open-menu-btn");
-const navigationMenu = document.querySelector(".navigation-menu--modified-layout");
+const navigationMenu = document.querySelector("nav .navigation-menu");
 const modal = document.querySelector(".modal");
+const requestButton = document.querySelector("nav .request-invite-btn");
 
 for (const buttonSelector in toggleButtons) {
-        const button = toggleButtons[buttonSelector];
+    const button = toggleButtons[buttonSelector];
 
-        button.addEventListener("click", ()=>{
-            toggleVisibility(toggleButtons[".close-menu-btn"], toggleButtons[".open-menu-btn"], modal, navigationMenu);
-            modal.classList.contains("hidden") ? document.body.style.overflow = "scroll": document.body.style.overflow = "hidden";
-        })
+    button.addEventListener("click", () => {
+        toggleVisibility(toggleButtons[".close-menu-btn"], toggleButtons[".open-menu-btn"], modal, navigationMenu);
+        modal.classList.contains("hidden") ? document.body.style.overflow = "scroll" : document.body.style.overflow = "hidden";
+    })
 }
+
+window.onload = () => {
+    changeLayout(navigationMenu, "hidden", "navigation-menu--modified-layout", "navigation-menu--white-bg");
+    changeLayout(requestButton, "hidden");
+}
+
+window.addEventListener("resize", ()=>{
+    changeLayout(navigationMenu, "hidden", "navigation-menu--modified-layout", "navigation-menu--white-bg");
+    changeLayout(requestButton, "hidden");
+})
--- JS-Files/utility-functions.js
+++ JS-Files/utility-functions.js
@@ -1,14 +1,21 @@
 /*
 Just wanted to practice ES6 modules
 */
-export const selectAll = (...selectors)=>{
-    return Object.fromEntries(selectors.map(currentSelector =>{
+export const selectAll = (...selectors) => {
+    return Object.fromEntries(selectors.map(currentSelector => {
         const targetElement = document.querySelector(currentSelector);
 
         return [currentSelector, targetElement];
     }));
 };
 
-export const toggleVisibility = (...elements)=>{
+export const toggleVisibility = (...elements) => {
     elements.forEach(element => element.classList.toggle("hidden"));
+}
+
+export const changeLayout = (targetElement, ...classes) => {
+    const navbarWidth = window.innerWidth;
+    classes.forEach((currentClass) => {
+        targetElement.classList.toggle(currentClass, navbarWidth < 768);
+    })
 }
