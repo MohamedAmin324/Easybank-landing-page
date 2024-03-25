7cd7543f2cfd7efa54d1b01cb3a1846c5087a7d9
MohamedAmin324 <mohamedaminebofficiel@gmail.com>
2024-03-25 13:06:46 +0100

    Fix footer menu links order

--- JS-Files/index.js
+++ JS-Files/index.js
@@ -3,12 +3,17 @@
 import { selectAll, toggleVisibility, changeLayout } from "./utility-functions.js";
 
 const [closeBtn, openBtn, navigationMenu, modal, requestButton, bgDesktop] = selectAll(".close-menu-btn", ".open-menu-btn", "nav .navigation-menu", ".modal", "nav .request-invite-btn", ".bg-desktop");
-
+const websiteLinks = document.querySelectorAll("a:not(.attribution a)");
 const toggleButtons = [closeBtn, openBtn];
+
 toggleButtons.forEach((btn) => {
     btn.addEventListener("click", () => {
         toggleVisibility(closeBtn, openBtn, modal, navigationMenu);
     })
+})
+
+websiteLinks.forEach((link) => {
+    link.addEventListener("click", (e) => e.preventDefault());
 })
 
 window.onload = () => {
